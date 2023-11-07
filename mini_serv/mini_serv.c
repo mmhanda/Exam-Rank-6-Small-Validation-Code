2e581f3c57c35406f26208b66d018756f2e146e8
Mohammed Handa <mohammedhanda109@gmail.com>
2023-11-07 17:36:04 +0100

    siplify the logic

--- mini_serv/mini_serv.c
+++ mini_serv/mini_serv.c
@@ -16,41 +16,41 @@ void exitError(char *str) {
     exit(1);
 }
 
-void sendAll(int sender_fd) {
+void sendMsg(int sender_fd) {
     for(int fd = 0; fd <= max; fd++)
         if (FD_ISSET(fd, &fds_Write) && fd != sender_fd) write(fd, buffWrite, strlen(buffWrite));
 }
 
 int main(int ac, char **av) {
     if (ac != 2) exitError("Wrong number of arguments\n");
-  	int sockfd = max = socket(AF_INET, SOCK_STREAM, 0); // copy from main.c Unitl line 27
-	if (sockfd == -1) exitError("Fatal error\n");
+  	int sockfd = max = socket(AF_INET, SOCK_STREAM, 0); // copy from subject/main.c Unitl line 27
+	if (sockfd == -1) exitError("Fatal error\n");      // copy
     FD_ZERO(&active_fds);
     FD_SET(sockfd, &active_fds);
-	struct sockaddr_in servaddr; // copy from main.c Unitl line 35
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
-	servaddr.sin_port = htons(atoi(av[1]));
-	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) exitError("Fatal error\n");
-	if (listen(sockfd, 128) != 0) exitError("Fatal error\n");  // it is 10 in the main replace it with 128
+	struct sockaddr_in servaddr; // copy from subject/main.c Unitl line 35
+	servaddr.sin_family = AF_INET; // copy
+	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1 // copy
+	servaddr.sin_port = htons(atoi(av[1]));                  // copy
+	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) exitError("Fatal error\n"); // copy
+	if (listen(sockfd, 128) != 0) exitError("Fatal error\n");  // replace 10 with 128 // copy
     while(1) {
         fds_Read = fds_Write = active_fds;
         if (select(max + 1, &fds_Read, &fds_Write, NULL, NULL) < 0) continue;
         for(int fd = 0; fd <= max; fd++) {
-            if (FD_ISSET(fd, &fds_Read) && fd == sockfd) {
+            if (FD_ISSET(fd, &fds_Read) && fd == sockfd) { // hnaya sorry in english join new client
                 int newfd = accept(sockfd, NULL, NULL);
                 max = (newfd > max) ? newfd : max;
                 clients[newfd].id = next_id++;
                 FD_SET(newfd, &active_fds);
                 sprintf(buffWrite, "server: client %d just arrived\n", clients[newfd].id);
-                sendAll(newfd);
+                sendMsg(newfd);
                 break;
             }
-            if (FD_ISSET(fd, &fds_Read) && fd != sockfd) {
+            if (FD_ISSET(fd, &fds_Read) && fd != sockfd) { // recive msgs from clients if its empty than the client is quit
                 int read = recv(fd, buffRead, 100000, 0);
                 if (read <= 0) {
                     sprintf(buffWrite, "server: client %d just left\n", clients[fd].id);
-                    sendAll(fd);
+                    sendMsg(fd);
                     bzero(clients[fd].msg, strlen(clients[fd].msg));
                     FD_CLR(fd, &active_fds);
                     close(fd);
@@ -62,7 +62,7 @@ int main(int ac, char **av) {
                         if (clients[fd].msg[j] == '\n') {
                             clients[fd].msg[j] = '\0';
                             sprintf(buffWrite, "client %d: %s\n", clients[fd].id, clients[fd].msg);
-                            sendAll(fd);
+                            sendMsg(fd);
                             bzero(clients[fd].msg, strlen(clients[fd].msg));
                             j = -1;
                         }
